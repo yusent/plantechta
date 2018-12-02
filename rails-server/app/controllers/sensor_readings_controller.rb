ca1464a4233087cc18feb462f3622e226c78f3b5
Jorge Luis Martinez Gomez <jol@fastmail.com>
2018-12-01 16:12:00 -0800

    make routes and controller methods

--- rails-server/app/controllers/sensor_readings_controller.rb
+++ rails-server/app/controllers/sensor_readings_controller.rb
@@ -1,2 +1,40 @@
 class SensorReadingsController < ApplicationController
+  def create
+    reading = SensorReadings.create sensor_readings_params
+    response = {
+      data: reading.as_json
+    }
+    response[:errors] = reading.errors.as_json unless reading.errors.empty?
+    render json: response
+  end
+
+  def index
+    response = SensorReadings.page(page, per_page).as_json
+    render json: response
+  end
+
+  def all_last
+    render json: {
+      data: {
+        humidity: HumidityReading.last.value,
+        temperature: TemperatureReading.last.value,
+        soil_moisture: SoilMoistureReading.last.value,
+        sunlight: SunlightReading.last.value,
+      }
+    }
+  end
+
+  private
+
+  def page
+    params[:page]&.to_i || 1
+  end
+
+  def per_page
+    params[:per_page]&.to_i || 20
+  end
+
+  def sensor_readings_params
+    params.permit :type, :value
+  end
 end
--- rails-server/config/routes.rb
+++ rails-server/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope :api do
+    get "sensor_readings", to: "sensor_readings#index"
+    post "sensor_readings", to: "sensor_readings#create"
+    get "sensor_readings/all_last", to: "sensor_readings#all_last"
+  end
 end
